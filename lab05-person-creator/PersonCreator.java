d5fee53f5c0a52f3e845e967c8588e88a74513a0
KINMEL WILLIAMS <kinmelwilliams@adelphi.internal>
2016-09-15 12:00:45 -0400

    almost finished lab05

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -8,4 +8,76 @@
 public class PersonCreator
 {
     // Your class begins here!
+    private int agenumber;
+    private String height;
+    private int weight;
+    private String name;
+    private String address;
+    private int ssn;
+    private int phonenumber;
+    private String dob;
+    
+    public int agenumber (int age);
+    {
+        agenumber = age;
+        if (age > 18 || age < 50) 
+        {
+           System.out.println("Age is "age"") 
+        }
+    }
+    
+    public String height (int feet, int inches);
+    {
+        height = feet + "-" + inches;
+    }
+    
+    public int weight (int pounds); 
+    {
+        weight = pounds;
+    }
+    public String name (String first, String last);
+    {
+        name = "first" + "last";
+    }
+    
+    public String address (int streetnb, String streetnm);
+    {
+        address = streetnb + "streetnm";
+    }
+    
+    public int ssn (int begin, int middle, int end);
+    {
+        ssn = begin + "-" middle "-" + end;
+        if (age > 999)
+        {
+            System.out.println("Use a lower number")
+        }
+        else if (begin > 999)
+        {
+            System.out.println("Use a lower number")
+        }
+        
+        else if (end > 9999)
+        {
+            System.out.println("Use a lower number")
+        }
+    } 
+    
+    public int phonenumber (int stanzaA, int stanzaB, int stanzaC);
+    { 
+        phonenumber = stanzaA + "-" + stanzaB + "-" + stanzaC;
+        if (stanzaA > 999)
+        {
+            System.out.println("Use a lower number")
+        }
+        else if (stanzaB > 999)
+        {
+            System.out.println("Use a lower number")
+        }
+        else (stanzaC > 9999) 
+        {
+            System.out.println("Use a lower number")
+        }
+    }
 }
+
